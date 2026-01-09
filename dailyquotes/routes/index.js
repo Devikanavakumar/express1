4db386b024b4c68cbcddabd9e05119e7ce2f42e7
Devikanavakumar <devikanavakumar@gmail.com>
2026-01-09 22:40:13 +0530

    Add shop application with product management features, including MongoDB integration and basic routing

--- dailyquotes/routes/index.js
+++ dailyquotes/routes/index.js
@@ -2,12 +2,10 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
-// MongoDB connection
 mongoose.connect("mongodb://127.0.0.1:27017/shopDB")
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-// Product schema
 const productSchema = new mongoose.Schema({
   name: String,
   price: Number,
@@ -16,12 +14,12 @@ const productSchema = new mongoose.Schema({
 
 const Product = mongoose.model("Product", productSchema);
 
-/* GET form */
+
 router.get('/', function(req, res) {
   res.render('index', { message: null });
 });
 
-/* POST form */
+
 router.post('/add-product', async function(req, res) {
   const { name, price, description } = req.body;
 
---
+++ shop/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/shopStock")
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.get("/", async (req, res) => {
+  const products = await Product.find();
+  res.render("index", {
+    products,
+    total: products.length
+  });
+});
+
+app.post("/add-product", async (req, res) => {
+  const { name, quantity, price } = req.body;
+  await Product.create({ name, quantity, price });
+  res.redirect("/");
+});
+
+module.exports = app;
